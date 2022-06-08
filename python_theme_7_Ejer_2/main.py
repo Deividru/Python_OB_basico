a9cb6a87dfdfd15094bd6ad003798038163bd8bc
david <davidrubianoare@gmail.com>
2022-06-08 04:24:20 +0200

    1

---
+++ python_theme_7_Ejer_2/main.py
@@ -0,0 +1,38 @@
+import time
+
+def main():
+   hora=4
+   min=23
+   #hora = int(input("introduzca la hora de salida: "))
+   #min  = int(input("introduzca los minutos de la hora de salida: "))
+
+   l = time.localtime()
+
+   print("Actualmente es : ",time.ctime(),"\n")
+
+   check(l,hora,min)
+
+def check(l,hora,min):
+
+   if l.tm_hour < hora or (l.tm_hour== hora and l.tm_min < min):
+
+      print("Aun no es la hora de salida (", hora, "h :",min, "m )\n")
+
+      if l.tm_min == min:
+         print("Quedan ", hora - l.tm_hour, "horas", 0, "minutos")
+      else:
+         if l.tm_min < min:
+            print("Quedan ",abs(l.tm_hour - (hora)), "horas", (min - l.tm_min), "minutos","para la hora de salida")
+         else:
+            print("Quedan ", abs(l.tm_hour - (hora -1)), "horas", abs(60 + min - l.tm_min), "minutos","para la hora de salida")
+   else:
+
+      if l.tm_hour > hora or (l.tm_hour== hora and l.tm_min > min):
+         print("La hora introducida como salida es anterior a la actual por lo que ya puedes irte")
+      else:
+         print("es Exactamente la hora de salida")
+
+
+main()
+
+
